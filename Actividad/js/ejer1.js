23d5d1d1d93562280e6ab402e5fa423223f994da
DanielBernal133 <82922690+DanielBernal133@users.noreply.github.com>
2021-06-28 23:13:22 -0500

    Add files via upload

---
+++ Actividad/js/ejer1.js
@@ -0,0 +1,16 @@
+//-------------EJERCICIO 1---------------//
+console.log (" //Par e impar// ")
+
+function par (){
+    var valor1 = document.getElementById('text1').value;
+    var porcentaje = valor1%2; 
+    if (valor1 %2 ==0) {
+        alert("El número es par");  
+      }else {
+        alert("El número es inpar");
+      }
+
+}
+
+
+
