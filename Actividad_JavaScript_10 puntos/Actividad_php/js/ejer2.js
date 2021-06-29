f5d2cf89b6ad15cb80a9c876e17b300e05786314
DanielBernal133 <82922690+DanielBernal133@users.noreply.github.com>
2021-06-28 23:12:42 -0500

    Delete Actividad_JavaScript_10 puntos/Actividad_php directory

--- Actividad_JavaScript_10 puntos/Actividad_php/js/ejer2.js
+++
@@ -1,8 +0,0 @@
-//-------------EJERCICIO 2---------------// 
-
-var tabla = prompt("¿Qué tabla desea que se imprimida?");
-
-for (var i = 1; i < 11; i++) {
-document.write(tabla + " x " + i + " = " +(tabla*i) + "<br>" );
-    
-}
--- Actividad_JavaScript_10 puntos/Actividad_php/js/ejer3.js
+++
@@ -1,9 +0,0 @@
-
-
-
-for (var i = 2; i < 10; i++) {
-    for (var e = 1; e < 10; e++) {
-document.write(i + " x " + i + " = " +(e*i) + "<br>" );
-    }
-    
-}
